0b2ed4369b59fca371bab0f8a36d5d108340b301
Vaibhav Rai <raivaibhav08@gmail.com>
2018-08-28 18:18:25 +0530

    Additional feature for HEROKU

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn scrapgsoc.wsgi --log-file -
--- scrapgsoc/settings.py
+++ scrapgsoc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#!@7)o7dt-7%qymv!b5!sb2f)zr-dp35f98sssvtelfl33ht&1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -125,3 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
