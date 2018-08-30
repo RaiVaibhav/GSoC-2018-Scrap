a9c4b28ff190d7b42fe22f03824081cbab1fe99d
Vaibhav Rai <raivaibhav08@gmail.com>
2018-08-30 16:34:24 +0530

    Fix Bug

--- scrap/admin.py
+++ scrap/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Organization, Project, Technology
+from .models import Organization, Project, Technology, Topic
 
 admin.site.register(Organization)
 admin.site.register(Project)
 admin.site.register(Technology)
+admin.site.register(Topic)
