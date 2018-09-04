5f0d74d14437d34d8191bc5502e544c3708f9187
Vaibhav Rai <raivaibhav08@gmail.com>
2018-09-04 21:37:09 +0530

    Fix Bug

--- scrap/views.py
+++ scrap/views.py
@@ -4,8 +4,9 @@ from django.db.models import Count
 from django.shortcuts import redirect
 
 def tag_display(request):
+    # import pdb; pdb.set_trace()
     item  = Technology.objects.order_by('name')
-    all_org = Organization.objects.annotate(num_projects=Count('project')).order_by('-num_projects', 'name')
+    all_org = Organization.objects.annotate(num_projects=Count('projects')).order_by('-num_projects', 'name')
     technology_tags = None
     if request.method == 'POST' and 'item_id' in request.POST:
         if 'Completed Projects' not in request.POST.get('item_id'):
@@ -16,7 +17,7 @@ def tag_display(request):
                 all_org = all_org.filter(technology_tags=my_object)
             technology_tags = [i.name for i in technology_objects]
             # all_org = Organization.objects.filter(technology_tags=technology_object).annotate(num_projects=Count('project')).order_by('-num_projects','name')
-            all_org = all_org.annotate(num_projects=Count('project')).order_by('-num_projects','name')
+            all_org = all_org.annotate(num_projects=Count('projects')).order_by('-num_projects','name')
     return render(request, 'scrap/base.html', {'tags':item, 'all_org': all_org,
         'group_name': request.POST.get('item_id'),
         'technology_tag': technology_tags})
