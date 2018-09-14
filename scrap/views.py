672c66276519ca3309b9e77612c431c8e27e74da
Vaibhav Rai <raivaibhav08@gmail.com>
2018-09-14 13:22:59 +0530

    Add feature

--- scrap/views.py
+++ scrap/views.py
@@ -7,6 +7,7 @@ def tag_display(request):
     # import pdb; pdb.set_trace()
     item  = Technology.objects.order_by('name')
     all_org = Organization.objects.annotate(num_projects=Count('projects')).order_by('-num_projects', 'name')
+    backup_all_org = all_org
     technology_tags = None
     if request.method == 'POST' and 'item_id' in request.POST:
         if 'Completed Projects' not in request.POST.get('item_id'):
@@ -15,6 +16,8 @@ def tag_display(request):
             technology_objects = Technology.objects.filter(id__in = tags_id)
             for my_object in technology_objects:
                 all_org = all_org.filter(technology_tags=my_object)
+            if len(all_org)==0:
+                all_org=backup_all_org.filter(technology_tags__in=technology_objects)
             technology_tags = [i.name for i in technology_objects]
             # all_org = Organization.objects.filter(technology_tags=technology_object).annotate(num_projects=Count('project')).order_by('-num_projects','name')
             all_org = all_org.annotate(num_projects=Count('projects')).order_by('-num_projects','name')
